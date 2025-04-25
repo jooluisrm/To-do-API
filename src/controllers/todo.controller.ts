5daf29fad921b3b7f3fcd31978a59af265f46b9b
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-04-25 10:03:09 -0300

    configurando o projeto para o mysql

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
 import { Todo } from '../models/Todo';
 
-export const all = async () => {
-
+export const all = async (req: Request, res: Response) => {
+    const list = await Todo.findAll();
+    res.json({
+        list
+    });
 }
 
 export const add = async () => {
